a1e9b1dffa13e6867b92b6d22033672d78d18914
johnpapaz <142339097+johnpapaz@users.noreply.github.com>
2024-10-09 00:33:47 -0500

    Lo hice ya todo
    Lo hice ya todo

--- co.edu.uniquindio.patronesDiseno/src/main/java/co/edu/uniquindio/patronesDiseno/Prototype/Main.java
+++ co.edu.uniquindio.patronesDiseno/src/main/java/co/edu/uniquindio/patronesDiseno/Prototype/Main.java
@@ -2,6 +2,56 @@ package co.edu.uniquindio.patronesDiseno.Prototype;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Cuchara cucharaPrototipo = new Cuchara("Acero Inoxidable", "Grande");
+        Cuchillo cuchilloPrototipo = new Cuchillo("Plata", "Mediano");
+        Tenedor tenedorPrototipo = new Tenedor("Plástico", "Pequeño");
+
+        FabricaCubiertos fabrica = new FabricaCubiertos(
+                    cucharaPrototipo,
+                    cuchilloPrototipo,
+                    tenedorPrototipo
+        );
+
+        System.out.println("Cubiertos originales:");
+        ICubierto cuchara1 = fabrica.crearCuchara();
+        ICubierto cuchillo1 = fabrica.crearCuchillo();
+        ICubierto tenedor1 = fabrica.crearTenedor();
+
+        cuchara1.usar();
+        cuchillo1.usar();
+        tenedor1.usar();
+
+        System.out.println("\nCubiertos personalizados:");
+
+        ICubierto cuchara2 = fabrica.crearCuchara();
+        cuchara2.usar();
+        ((Cuchara) cuchara2).setMaterial("Oro");
+        ((Cuchara) cuchara2).setTamaño("Pequeña");
+        cuchara2.usar();
+
+        ICubierto cuchillo2 = fabrica.crearCuchillo();
+        cuchillo2.usar();
+        ((Cuchillo) cuchillo2).setMaterial("Acero al Carbono");
+        ((Cuchillo) cuchillo2).setTamaño("Grande");
+        cuchillo2.usar();
+
+        ICubierto tenedor2 = fabrica.crearTenedor();
+        tenedor2.usar();
+        ((Tenedor) tenedor2).setMaterial("Titanio");
+        ((Tenedor) tenedor2).setTamaño("Mediano");
+        tenedor2.usar();
+
+        ICubierto cuchillo3 = fabrica.crearCuchillo();
+        ((Cuchillo) cuchillo3).setMaterial("Cerámica");
+        ((Cuchillo) cuchillo3).setTamaño("Pequeño");
+        cuchillo3.usar();
+
+        ICubierto tenedor3 = fabrica.crearTenedor();
+        ((Tenedor) tenedor3).setMaterial("Acero Inoxidable");
+        ((Tenedor) tenedor3).setTamaño("Grande");
+        tenedor3.usar();
+
     }
 }
+
